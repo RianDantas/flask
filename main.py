6bedc343dea7966c8a46dd66887d2ecae4317272
RianDantas <134387076+RianDantas@users.noreply.github.com>
2025-02-07 18:24:54 +0000

    MUDAR SENHA

--- main.py
+++ main.py
@@ -15,6 +15,27 @@ def helo_world():
 # Vaiavel global para cadastro de usuarios
 usuarios = []
 
+
+def gerirLogin(login):
+    print(login)
+    print("gerir Login")
+    for i in usuarios:
+
+        if i["login"] == login:
+            print("cadastrado")
+            return False
+        else:
+            print("nao cadastrado")
+            return True
+
+     
+
+
+
+
+
+
+
 # Rota para o html de cadastrar usuario
 @app.route("/cadastro")
 def cadastro():
@@ -30,8 +51,14 @@ def cadastroUsuarios():
     nome = request.form.get("nome")
     login = request.form.get("login")
     senha = request.form.get("senhaUsuario")
-    usuarios.append({"nome": nome, "login": login, "senha": senha})
-    print(usuarios)
+
+    logado = gerirLogin(login)
+    print("cadastroUsuarios")
+    print(logado)
+
+    if logado == True:
+        usuarios.append({"nome": nome, "login": login, "senha": senha})
+        print(usuarios)
     return  render_template("index.html")
     
 
@@ -71,8 +98,34 @@ def recuperacaoDeSenha():
             return render_template("recuperarSenha.html", message = message)
 
 
+@app.route("/listarUsuarios")
+def listaUsarios():
+    return render_template("lista.html", lista=usuarios)
 
 
+
+@app.route('/paginaMudarSenha')
+def paginaRecuperarSenha():
+    return render_template("paginaMudarSenha.html")
+
+@app.route('/mudarSenha', methods=['POST'])
+def mudarSenha():
+    nome = request.form.get('nome')
+    email = request.form.get('login')
+    senha = request.form.get('senhaUsuario')
+
+    for i in usuarios:
+
+        if i["nome"] == nome and i["login"] == email:
+            i["senha"] = senha
+            print(i["senha"])
+            message = "Senha modificada com sucesso!: " + i["senha"]
+            return render_template("paginaMudarSenha.html", message=message)
+
+        else:
+            message = "nao existe"
+            return render_template("paginaMudarSenha.html", message = message)
+    
 
 
 
